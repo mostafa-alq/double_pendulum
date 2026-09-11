89dc2304525bba8c9472a97da0c9cc6a318b3165
mostafa-alq <mostafaalqadi05@gmail.com>
2026-09-11 13:47:25 +0100

    Creating test neural network before creating full scale implementation

--- neural.py
+++ neural.py
@@ -1,1 +1,19 @@
-# import neural network
+# Test neural network setup
+arr = [[0, 1],[1, 3], [2,5], [3,7],[4,9]]
+
+w = 0
+b = 0
+
+def predict(x, w, b):
+    return w * x + b
+
+def loss():
+    err_arr = []
+    for i in range(0, len(arr)):
+        y_predict = predict(arr[i][0], w, b)
+        err_arr.append((arr[i][1] - y_predict) ** 2)
+
+    mse = sum(err_arr) / len(err_arr)
+    return mse
+
+print(loss())
